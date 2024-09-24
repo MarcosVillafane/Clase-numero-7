0f51e928bc671f9ca0cf4704bfc20baf0c0b78fd
c00villamj <marvilla12@publicisgroupe.net>
2024-09-24 09:44:54 -0300

    Add .name, .gitignore, styles.css, version.ini, Prueba.java, Pruebaparamigracion.userlibraries, HolaMundo.java, javaLikeNames.txt, introstate, OpenTypeHistory.xml, history.version, dialog_settings.xml, properties.version, org.eclipse.ui.prefs, QualifiedTypeNameHistory.xml, .classpath, PruebaIntellijen.eml, misc.xml, org.eclipse.core.resources.prefs, org.eclipse.urischeme.prefs, 0.log, org.eclipse.m2e.discovery.prefs, org.eclipse.jsch.core.prefs, org.eclipse.mylyn.context.core.prefs, org.eclipse.ui.ide.prefs, modules.xml, org.eclipse.mylyn.monitor.ui.prefs, index.html, workspace.setup, and org.eclipse.core.resources.prefs files

---
+++ repasoSnippets/app.java
@@ -0,0 +1,9 @@
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hola a todos");
+    System.out.println("Este es un mensaje de prueba");
+    System.out.println("Para ver si funciona el sistema de control de versiones");
+    System.out.println("y finalizar nuestro curso de VSCode");
+    System.out.println("Gracias por su atención");
+  }
+}
